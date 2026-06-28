96c834233bbc946686954b6234a9ab094a9196fd
Thomas4182 <109043811+Thomas4182@users.noreply.github.com>
2026-06-28 15:31:09 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'clock-planner-v1';
+const ASSETS = ['./', './My%20Daily_planner%20Noti.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))))
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then((cached) => cached || caches.match('./My%20Daily_planner%20Noti.html')))
+  );
+});
